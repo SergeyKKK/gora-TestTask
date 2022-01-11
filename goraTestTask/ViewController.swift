ac679db48cc19911f00922570c011a655a9ef824
SergeyKKK <skusnarev3@gmail.com>
2022-01-12 00:03:11 +0300

    Initial Commit

---
+++ goraTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  goraTestTask
+//
+//  Created by Serega Kushnarev on 12.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
